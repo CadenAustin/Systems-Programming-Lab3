4f21206eae37306d6e87bcbe7641a289b054af0c
Caden Austin <cadenbryant@hotmail.com>
2021-03-30 19:44:56 -0500

    Almost there

--- lab3.c
+++ lab3.c
@@ -9,22 +9,48 @@
 
 typedef struct{
     int free;
+    pid_t processID;
     int pipeFDS[2];
+    char *fileName;
 } PipeDictionary;
 
 void print_usage(){
-    fprintf(stdout, "Usage: runpar NUMCORES COMMAND... _files_ FILE...\n");
+    fprintf(stderr, "Usage: runpar NUMCORES COMMAND... _files_ FILE...\n");
     return;
 }
 
 
-void run_command_in_subprocess(char *file, int writeFD){
-    printf("Hello, %s %d\n", file, writeFD);
-    return;
-}
-
-void printout_terminated_subprocess(int status){
+pid_t run_command_in_subprocess(char *file, char *command[], int fileIndex, int writeFD){
+    pid_t currentWorkingPID = fork();
+    command[fileIndex] = file;
+    if (currentWorkingPID == -1){
+        fprintf(stderr, "Problem with fork on file: %s\n", file);
+        return 0;
+    } else if (currentWorkingPID == 0) {
+        dup2(writeFD, 1);
+        if ((execvp(command[0], command)) < 0){
+            fprintf(stderr, "Problem with exec on file: %s\n", file);
+            return 0;
+        }
+    }
+    return currentWorkingPID;
     
+}
+
+int printout_terminated_subprocess(int status, PipeDictionary *dictionaryItem, int *errorInRun){ 
+    if (WEXITSTATUS(status) == EXIT_FAILURE){
+        errorInRun++;
+        return -1;
+    }
+
+   return 0;
+}
+
+void print_header(char *name){
+    printf("____________________");
+    printf("Output from: %s", name);
+    printf("--------------------");
+    return;
 }
 
 int get_free_pipe(PipeDictionary *pipeManagment, int size){
@@ -36,9 +62,27 @@ int get_free_pipe(PipeDictionary *pipeManagment, int size){
     return -1;
 }
 
+int all_pipes_accounted(PipeDictionary *pipeManagment, int size){
+    for (int x = 0; x < size; x++){
+        if (pipeManagment[x].free == 1){
+            return -1; 
+        }
+    } 
+    return 0;
+}
+
+int match_pid_with_struct(PipeDictionary *pipeManagment, int size, pid_t searchPID){
+    for (int x = 0; x < size; x++){
+        if (pipeManagment[x].processID == searchPID){
+            return x; 
+        }
+    } 
+    return -1;
+}
+
 int main(int argc, char *argv[]){
     char *command[argc];
-    int commandIndex = 0, argIndex = 1, numberOfCores = 0;
+    int status, errorInRun = 0, commandIndex = 0, argIndex = 1, numberOfCores = 0;
 
     if (argc <= 4){
         print_usage();
@@ -59,7 +103,7 @@ int main(int argc, char *argv[]){
         commandIndex++;
     }
      
-    command[commandIndex] = "\0";
+    command[commandIndex + 1] = NULL;
     
     PipeDictionary pipeManagment[numberOfCores];
     for (int x = 0; x < numberOfCores; x++){
@@ -70,19 +114,28 @@ int main(int argc, char *argv[]){
 
     for (int fileIndex = argIndex+1; fileIndex < argc; fileIndex++){
         
-        printf("Testing"); 
-        int pipeIndex = get_free_pipe(pipeManagment, numberOfCores);
+        int dictIndex = get_free_pipe(pipeManagment, numberOfCores);
     
-        if (pipeIndex == -1){
-            printf("waiting");
-            exit(EXIT_SUCCESS);
+        if (dictIndex == -1){
+            //Wait
         }
-        
-        printf("%d", pipeIndex);
-        pipe(pipeManagment[pipeIndex].pipeFDS);
-        pipeManagment[pipeIndex].free = 1;
-        
-        run_command_in_subprocess(argv[fileIndex], pipeManagment[pipeIndex].pipeFDS[1]);
+        PipeDictionary workingDict = pipeManagment[dictIndex];
+        pipe(workingDict.pipeFDS);
+        workingDict.free = 1;
+         
+        pid_t currentWorkingPID = run_command_in_subprocess(argv[fileIndex], command, commandIndex, workingDict.pipeFDS[1]);
+        if (currentWorkingPID == 0){
+            exit(EXIT_FAILURE);
+        }
+        workingDict.processID = currentWorkingPID; 
+        workingDict.fileName = argv[fileIndex];
     }
+
+    while (all_pipes_accounted(pipeManagment, numberOfCores) != -1){
+        pid_t finishedProcess = wait(&status);
+        int dictIndex = match_pid_with_struct(pipeManagment, numberOfCores, finishedProcess);
+        printout_terminated_subprocess(status, &pipeManagment[dictIndex], &errorInRun);
+        break;
+    } 
     
 }
